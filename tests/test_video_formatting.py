e5e41f9877d605ae824a49db9b99b1b940d223c3
Rao Abdul <raoabdul@Raos-Laptop.local>
2024-08-20 22:22:16 +0400

    Added video formatting file base code ✅

---
+++ tests/test_video_formatting.py
@@ -0,0 +1,13 @@
+import re
+from rich.prompt import Prompt
+from rich.traceback import install
+install(show_locals=True)
+
+from rich import print
+from rich.panel import Panel
+
+import os
+import subprocess
+
+def convert_video_aspect_ratio(video_path, output_path):
+    ...
